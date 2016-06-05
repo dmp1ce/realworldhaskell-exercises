e67d759ae08465098258afd83c0186bef02fafd4
David <daveparrish@gmail.com>
2016-06-05 00:13:46 -0400

    Add exercise 8

--- defining-types-streamlining-functions/Tree.hs
+++ defining-types-streamlining-functions/Tree.hs
@@ -11,3 +11,22 @@ data TreeM a  = NodeM a
                     deriving (Show)
 
 maybeTree = NodeM "Start" (Just (NodeM "two" Nothing Nothing)) Nothing
+
+-- Exercise 8
+height :: Tree a -> Int
+height Empty                    = 0
+height (Node _ Empty Empty)     = 1
+height (Node _ x y)
+  | (height x) >= (height y)    = 1 + height x
+  | otherwise                   = 1 + height y
+
+
+heightIs2 = height aTree == 2
+
+longerTree = Node "hello"
+  (Node "another" Empty Empty) (Node "another"  -- 2
+  (Node "3"                                     -- 3
+  (Node "4"                                     -- 4
+   Empty Empty) Empty) Empty)
+
+heightIs4 = height longerTree == 4
